3e18286fff8633ceec94afe9b7ed6b6c1fb9f9fc
Nolan Reilly <nolanreilly02@gmail.com>
2024-02-10 02:58:18 -0600

    Add graphs to dashboard

--- version-node/app.js
+++ version-node/app.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+let currPh = 7;
+let currTemp = 70;
+let currMoisture = 50;
+
+// Emit the updated data every second
+setInterval(() => {
+  currPh = updateValue(currPh, 0, 0.1, 0, 14);
+  currTemp = updateValue(currTemp, 0, 1, -50, 50);
+  currMoisture = updateValue(currMoisture, 0, 5, 0, 100);
+  console.log('PH:', currPh, 'Temp:', currTemp, 'Moisture:', currMoisture);
+
+  io.emit('data', { currPh, currTemp, currMoisture });
+}, 100);
 
 let deviceData = {};  // to store the form data
 
@@ -30,20 +46,22 @@ app.post('/submit', (req, res) => {
 });
 
 app.get('/dashboard', (req, res) => {
-  let phData = [deviceData.lowPH, deviceData.highPH];
-  let tempData = [deviceData.lowTemp, deviceData.highTemp];
-  let moistureData = [deviceData.lowMoisture, deviceData.highMoisture];
-
   res.render('dashboard', { 
-    phData: phData, tempData: tempData,
-    moistureData: moistureData,
+    phData: currPh,
+    tempData: currTemp,
+    moistureData: currMoisture,
     deviceName: deviceData.deviceName,
-    deviceId: deviceData.deviceId
+    deviceId: deviceData.deviceId,
   });
-
-  
 });
 
-app.listen(port, () => {
+// Function to update a value within a certain range
+function updateValue(value, minChange, maxChange, minValue, maxValue) {
+  const change = minChange + Math.random() * (maxChange - minChange);
+  value += Math.random() < 0.5 ? -change : change;
+  return Math.max(minValue, Math.min(maxValue, value));
+}
+
+http.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
