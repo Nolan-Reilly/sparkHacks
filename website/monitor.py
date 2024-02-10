d891cf4be0765e4d50265cf4f367d7f6e637e88d
M-i-c-a-h <oolug4@uic.edu>
2024-02-10 13:42:50 -0600

    ...

--- website/monitor.py
+++ website/monitor.py
@@ -32,44 +32,50 @@ def send_notification(mail, body):
         print(e.message)
 
 # Monitor soil condition
-def monitor_soil_condition(smart_device):
+def monitor_soil_condition(SMART_Objs):
     while True:
-        if smart_device.soil_temp > smart_device.soil_high_temp_threshold:
-            message = f"{smart_device.Name} soil temperature is above the threshold value of {smart_device.soil_high_temp_threshold}!"
-            send_notification(smart_device.Email,message)
-        if smart_device.soil_temp < smart_device.soil_low_temp_threshold:
-            message = f"{smart_device.Name} soil temperature is below the threshold value of {smart_device.soil_low_temp_threshold}!"
-            send_notification(smart_device.Email,message)
-        if smart_device.soil_moisture > smart_device.soil_high_moisture_threshold:
-            message = f"{smart_device.Name} soil moisture is above the threshold value of {smart_device.soil_high_moisture_threshold}!"
-            print("Monitoring soil condition...")
-            print(message)
-            send_notification(smart_device.Email,message)
-        if smart_device.soil_moisture < smart_device.soil_low_moisture_threshold:
-            message = f"{smart_device.Name} soil moisture is below the threshold value of {smart_device.soil_low_moisture_threshold}!"
-            send_notification(smart_device.Email,message)
-        if smart_device.soil_ph > smart_device.soil_high_ph_threshold:
-            message = f"{smart_device.Name} soil pH is above the threshold value of {smart_device.soil_high_ph_threshold}!"
-            send_notification(smart_device.Email,message)
-        if smart_device.soil_ph < smart_device.soil_low_ph_threshold:
-            message = f"{smart_device.Name} soil pH is below the threshold value of {smart_device.soil_low_ph_threshold}!"
-            send_notification(smart_device.Email,message)
-        time.sleep(3600)
+        for smart_device in SMART_Objs:
+            if smart_device.soil_temp > smart_device.soil_high_temp_threshold:
+                message = f"{smart_device.Name} soil temperature is above the threshold value of {smart_device.soil_high_temp_threshold}!"
+                send_notification(smart_device.Email,message)
+            if smart_device.soil_temp < smart_device.soil_low_temp_threshold:
+                message = f"{smart_device.Name} soil temperature is below the threshold value of {smart_device.soil_low_temp_threshold}!"
+                send_notification(smart_device.Email,message)
+            if smart_device.soil_moisture > smart_device.soil_high_moisture_threshold:
+                message = f"{smart_device.Name} soil moisture is above the threshold value of {smart_device.soil_high_moisture_threshold}!"
+                print("Monitoring soil condition...")
+                print(message)
+                send_notification(smart_device.Email,message)
+            if smart_device.soil_moisture < smart_device.soil_low_moisture_threshold:
+                message = f"{smart_device.Name} soil moisture is below the threshold value of {smart_device.soil_low_moisture_threshold}!"
+                send_notification(smart_device.Email,message)
+            if smart_device.soil_ph > smart_device.soil_high_ph_threshold:
+                message = f"{smart_device.Name} soil pH is above the threshold value of {smart_device.soil_high_ph_threshold}!"
+                send_notification(smart_device.Email,message)
+            if smart_device.soil_ph < smart_device.soil_low_ph_threshold:
+                message = f"{smart_device.Name} soil pH is below the threshold value of {smart_device.soil_low_ph_threshold}!"
+                send_notification(smart_device.Email,message)
+            time.sleep(3600)
 
 def main():
     # Create SMART device
     f = open("/Users/pappi/VsCodes/sparkHacks/website/customer.txt", "r")
     str = f.read()
-    smart_device = SMART(str.split(',')[0], str.split(',')[1], str.split(',')[2], 
-                         int(str.split(',')[3]), int(str.split(',')[4]), 
-                         int(str.split(',')[5]), int(str.split(',')[6]), 
-                         int(str.split(',')[7]), int(str.split(',')[8])
-                    )
+    list = []
+    list = str.split('\n')
+
+    SMART_Objs = []
+    for i in list:
+        if i != '':
+            obj = i.split(',')
+            smart_device = SMART(obj[0], obj[1], obj[2], int(obj[3]), int(obj[4]), int(obj[5]), int(obj[6]), int(obj[7]), int(obj[8]))
+            SMART_Objs.append(smart_device)
+    
     # Monitor soil condition
 
     # test the monitor_soil_condition function
     #smart_device.soil_moisture = 70
-    monitor_thread = Thread(target=monitor_soil_condition, args=(smart_device,))
+    monitor_thread = Thread(target=monitor_soil_condition, args=(SMART_Objs,))
     monitor_thread.daemon = False
     monitor_thread.start()
 
