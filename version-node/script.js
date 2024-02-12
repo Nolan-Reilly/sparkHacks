a4c3f97eb61d2c44e1494618e18236aca9fc9bc8
Nolan Reilly <nolanreilly02@gmail.com>
2024-02-11 19:12:28 -0600

    Remove Node version of website

--- version-node/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  var menuItems = document.getElementById('mobile-menu-items');
-  if (menuItems.style.display === "none") {
-    menuItems.style.display = "block";
-  } else {
-    menuItems.style.display = "none";
-  }
-}
